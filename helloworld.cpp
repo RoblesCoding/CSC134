28f7e6fc55283a99adbb74c523c67c54e2a295a6
Elias Robles <elias.o.robles@gmail.com>
2026-01-13 17:27:03 +0000

    Completed m1t1

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,14 +1,20 @@
 /*
-elias robles
-csc-134
-first program - hello world 
+Elias Robles
+CSC-134
+First program - Hello world m1t1
 */
 
 #include <iostream>
 using namespace std; 
 
 int main () {
-
+    //Greet the world
+    cout << "Hello, world!" << endl;
+    cout << "What is your name?" << endl;
+    string name;
+    cin >> name;
+    cout << "Nice to meet you, " << name << "." << endl;
+    
 return 0; 
 
 }
