5b257bfac7ed0ea6f67112a1e16987672626897d
Elias Robles <elias.o.robles@gmail.com>
2026-01-13 17:52:30 +0000

    M1LAB

--- M1LAB.cpp
+++ M1LAB.cpp
@@ -9,16 +9,22 @@ using namespace std;
 
 int main(){
     string name = "Jane Smith";
-    cout << "Wellcome to " << name << "'s apple farm." endl;
+    cout << "Wellcome to " << name << "'s apple farm.";
+    endl;
     int apples = 225;
     double pricePerApple = 0.25;
 
-    cout << "We have " << int << "apples for sale." endl;
-    cout << "The price is $" << pricePerApple << "per apple." endl;
+    cout << "We have " << apples << "apples for sale.";
+    endl;
+    cout << "The price is $" << pricePerApple << "per apple.";
+    endl;
 
     //Calculating total price
     double totalPrice = (double) apples * pricePerApple;
     cout << "The total price is $" << totalPrice;
+    endl;
 
+
+return 0;
 
 }
