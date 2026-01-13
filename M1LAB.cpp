b35e74a0c070114d627eb835642bdaac62d3b491
Elias Robles <elias.o.robles@gmail.com>
2026-01-13 17:50:14 +0000

    M1LAB

---
+++ M1LAB.cpp
@@ -0,0 +1,24 @@
+/*
+Elias Robles
+CSC-134
+M1LAB Apple Sales
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    string name = "Jane Smith";
+    cout << "Wellcome to " << name << "'s apple farm." endl;
+    int apples = 225;
+    double pricePerApple = 0.25;
+
+    cout << "We have " << int << "apples for sale." endl;
+    cout << "The price is $" << pricePerApple << "per apple." endl;
+
+    //Calculating total price
+    double totalPrice = (double) apples * pricePerApple;
+    cout << "The total price is $" << totalPrice;
+
+
+}
