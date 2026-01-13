f05822bae39778c2be49404ec81b458e300b8f9d
Elias Robles <elias.o.robles@gmail.com>
2026-01-13 18:06:46 +0000

    Completed M1LAB

--- M1LAB.cpp
+++ M1LAB.cpp
@@ -8,21 +8,21 @@ M1LAB Apple Sales
 using namespace std;
 
 int main(){
+    //Store owner name
     string name = "Jane Smith";
-    cout << "Wellcome to " << name << "'s apple farm.";
-    endl;
-    int apples = 225;
+    cout << "Wellcome to " << name << "'s apple farm." << endl;
+    //Variable
+    int apples = 100;
+    //Price
     double pricePerApple = 0.25;
 
-    cout << "We have " << apples << "apples for sale.";
-    endl;
-    cout << "The price is $" << pricePerApple << "per apple.";
-    endl;
+    cout << "We have " << apples << " apples for sale." << endl;
+    cout << "The price is $" << pricePerApple << " per apple." << endl;
 
     //Calculating total price
     double totalPrice = (double) apples * pricePerApple;
-    cout << "The total price is $" << totalPrice;
-    endl;
+    //Display total
+    cout << "The total price is $" << totalPrice << "." << endl;
 
 
 return 0;
