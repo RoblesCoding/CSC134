827cd5ed1ce789d1babdb6010607fc9db770911f
Elias Robles <elias.o.robles@gmail.com>
2026-02-18 15:53:01 +0000

    M3LAB1 Draft1

--- .vscode/M3LAB.cpp
+++ .vscode/M3LAB.cpp
@@ -1,11 +1,15 @@
 /*
 Elias Robles
 CSC-134
-This program is a choice program
+M3LAB1
+This program is a choice program for a diplomatic meeting.
 */
 
 #include <iostream>
 using namespace std;
+
+void chooseChoice1();
+void chooseChoice2();
 
 int main(){
     //variables
@@ -16,16 +20,26 @@ int main(){
     cout << "Choice 1: Good evening. I await our discussions of peace and detente for mutual properity." << endl;
     cout << "Choice 2: Good evening. I hope to negotiate mutual detente, and have the the force to ensure it." << endl;
     cin >> choice;
-    if (1 == choice) {
-        chooseChoice1();
-    }
-    else if(2 == choice) {
-        chooseChoice2();
-    }
-    else {
-        cout << "Invalid option. Reinput choice 1 or 2."
-    }
+        if (1 == choice) {
+            chooseChoice1();
+        }
 
-    cout << "Thanks for playing," << endl;
+        else if(2 == choice) {
+            chooseChoice2();
+        }
+        
+        else {
+            cout << "Invalid option. Reinput choice 1 or 2.";
+        }
+    
+    cout << "Thanks for playing." << endl;
     return 0;
+}
+
+void chooseChoice1(){
+    cout << "The rival is pleased with your words. This was a fruitful meeting." << endl;
+}
+
+void chooseChoice2(){
+    cout << "The rival is displeased with your comment of force. This was not a fruitful meeting." << endl;
 }
